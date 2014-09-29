812822498ae05260b2b4d4cac24169a8b711446c
SniperElisia <danielu02@naver.com>
2014-09-29 22:39:31 +0900

    Version Change
    HAYO!

--- main/java/com/teamoort/oortech/OorTech.java
+++ main/java/com/teamoort/oortech/OorTech.java
@@ -17,7 +17,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class OorTech
 {
     public static final String MODID = "OorTech";
-    public static final String VERSION = "Alpha 0.0.0.1";
+    public static final String VERSION = "Alpha 0.0.0.001";
     
     @Metadata
     public ModMetadata meta;
