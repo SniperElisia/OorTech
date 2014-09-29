993f75dbba95c8de52aed0aeac8f7f04a6f5b81e
teamoort <teamoort@daum.net>
2014-09-29 19:04:59 +0900

    version

--- main/java/com/teamoort/oortech/OorTech.java
+++ main/java/com/teamoort/oortech/OorTech.java
@@ -15,7 +15,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class OorTech
 {
     public static final String MODID = "OorTech";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "Alpha 0.0.0.1";
     
     @Metadata
     public ModMetadata meta;
