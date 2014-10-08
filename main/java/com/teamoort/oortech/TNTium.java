b0c02d5a99cf56929aaf4196767421822d1349b5
teamoort <teamoort@daum.net>
2014-10-08 21:01:43 +0200

    수정TNTium

--- main/java/com/teamoort/oortech/TNTium.java
+++ main/java/com/teamoort/oortech/TNTium.java
@@ -1,8 +1,12 @@
 package com.teamoort.oortech;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.world.World;
 
 public class TNTium extends Block {
@@ -23,12 +27,29 @@ public class TNTium extends Block {
 
 	public void onBlockDestroyedByPlayer(World world, int x, int y, int z, int meta)
 	{
+		
+		
         if (!world.isRemote)
         {
-            EntityTNTPrimed entitytntprimed = new EntityTNTPrimed(world, (double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), null);
-            entitytntprimed.fuse = world.rand.nextInt(entitytntprimed.fuse / 4) + entitytntprimed.fuse / 8;
-            world.spawnEntityInWorld(entitytntprimed);
+        
+            	EntityTNTPrimed entitytntprimed = new EntityTNTPrimed(world, (double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), null);
+            	entitytntprimed.fuse = world.rand.nextInt(entitytntprimed.fuse / 4) + entitytntprimed.fuse / 8;
+            	world.spawnEntityInWorld(entitytntprimed);
+                        
+
         }
+        
 	}
 
+    public Item getItemDropped(int meta, Random rand, int fortune)
+    {
+        return Items.gunpowder;
+    }
+    
+    public int quantityDropped(Random rand)
+    {
+        return 1 + rand.nextInt(2);
+    }
+
+	
 }
