731641a83a39ee36675ee9daf731992dd90ea959
teamoort <teamoort@daum.net>
2015-01-04 19:49:18 +0900

    화약석 폭발 수정 + 애플래시의 자바 실력 증가

--- main/java/com/teamoort/redoxiation/blocks/TNTium.java
+++ main/java/com/teamoort/redoxiation/blocks/TNTium.java
@@ -29,17 +29,11 @@ public class TNTium extends Block {
 
 	public void onBlockDestroyedByPlayer(World world, int x, int y, int z, int meta)
 	{
-		
-		
-        if (!world.isRemote)
-        {
-        
-            	EntityTNTPrimed entitytntprimed = new EntityTNTPrimed(world, (double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), null);
-            	entitytntprimed.fuse = world.rand.nextInt(entitytntprimed.fuse / 4) + entitytntprimed.fuse / 8;
-            	world.spawnEntityInWorld(entitytntprimed);
-                        
-
-        }
+		Random random = new Random();
+		if(random.nextInt(4)>=3)
+		{
+			world.createExplosion(null, x, y, z, 2, true);
+		}
         
 	}
 
