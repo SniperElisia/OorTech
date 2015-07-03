5961f5af9c9377d0f37517a8905eaf3b7b8c89e8
말랑바람 <malangbalam@darklambs.net>
2015-07-03 20:58:09 +0900

    바꿀꺼면 한꺼번에 바꾸던가

--- main/java/teamoort/redoxiation/RedoxiationRecipeManager.java
+++ main/java/teamoort/redoxiation/RedoxiationRecipeManager.java
@@ -10,23 +10,23 @@ public class RedoxiationRecipeManager {
 	public static void recipeFurnace()
 	{
 		//Normal Smelting
-		GameRegistry.addSmelting(RedoxiationBlocks.CopperOre, new ItemStack(RedoxiationGenericItems.CopperIngot,1,0),0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.ChromiumOre, new ItemStack(RedoxiationGenericItems.ChromiumIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.CobaltOre, new ItemStack(RedoxiationGenericItems.CobaltIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.LeadOre, new ItemStack(RedoxiationGenericItems.LeadIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.NickelOre, new ItemStack(RedoxiationGenericItems.NickelIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.PlatinumOre, new ItemStack(RedoxiationGenericItems.PlatinumIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.SilverOre, new ItemStack(RedoxiationGenericItems.SilverIngot,1,0),0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.TinOre, new ItemStack(RedoxiationGenericItems.TinIngot,1,0),0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.ZincOre, new ItemStack(RedoxiationGenericItems.ZincIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.Bauxite, new ItemStack(RedoxiationGenericItems.AluminumIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.Rutile, new ItemStack(RedoxiationGenericItems.TitaniumIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(RedoxiationBlocks.Scheelite, new ItemStack(RedoxiationGenericItems.TungstenIngot,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.CopperOre, new ItemStack(RedoxiationGenericItems.IngotCopper,1,0),0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.ChromiumOre, new ItemStack(RedoxiationGenericItems.IngotChromium,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.CobaltOre, new ItemStack(RedoxiationGenericItems.IngotCobalt,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.LeadOre, new ItemStack(RedoxiationGenericItems.IngotLead,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.NickelOre, new ItemStack(RedoxiationGenericItems.IngotNickel,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.PlatinumOre, new ItemStack(RedoxiationGenericItems.IngotPlatinum,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.SilverOre, new ItemStack(RedoxiationGenericItems.IngotSilver,1,0),0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.TinOre, new ItemStack(RedoxiationGenericItems.IngotTin,1,0),0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.ZincOre, new ItemStack(RedoxiationGenericItems.IngotZinc,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.Bauxite, new ItemStack(RedoxiationGenericItems.IngotAluminum,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.Rutile, new ItemStack(RedoxiationGenericItems.IngotTitanium,1,0), 0.1f);
+		GameRegistry.addSmelting(RedoxiationBlocks.Scheelite, new ItemStack(RedoxiationGenericItems.IngotTungsten,1,0), 0.1f);
 		
 		//Dust Smelting
-		GameRegistry.addSmelting(new ItemStack(RedoxiationGenericItems.CopperDust,1,0), new ItemStack(RedoxiationGenericItems.CopperIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(new ItemStack(RedoxiationGenericItems.ChromiumDust,1,0), new ItemStack(RedoxiationGenericItems.ChromiumIngot,1,0), 0.1f);
-		GameRegistry.addSmelting(new ItemStack(RedoxiationGenericItems.CobaltDust,1,0), new ItemStack(RedoxiationGenericItems.CobaltIngot,1,0), 0.1f);
+		GameRegistry.addSmelting(new ItemStack(RedoxiationGenericItems.CopperDust,1,0), new ItemStack(RedoxiationGenericItems.IngotCopper,1,0), 0.1f);
+		GameRegistry.addSmelting(new ItemStack(RedoxiationGenericItems.ChromiumDust,1,0), new ItemStack(RedoxiationGenericItems.IngotChromium,1,0), 0.1f);
+		GameRegistry.addSmelting(new ItemStack(RedoxiationGenericItems.CobaltDust,1,0), new ItemStack(RedoxiationGenericItems.IngotCobalt,1,0), 0.1f);
 	}
 	
 	public static void recipeCrafting()
