aa501655d78349d6a1263b3e61507233f29d864d
alvin137 <alvin137@naver.coom>
2015-07-10 19:23:05 +0900

    뭘 수정했을까요?
    수정한게 아닙니다. 만들어 낸거죠
    커맨드 기초 짜놓음

---
+++ main/java/teamoort/redoxiation/cmd/CommandCommon.java
@@ -0,0 +1,68 @@
+package teamoort.redoxiation.cmd;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import teamoort.redoxiation.blocks.tileentity.TileEntityWoodenCog;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentTranslation;
+
+public class CommandCommon implements ICommand {
+	private List aliases;
+	private TileEntityWoodenCog wcog;
+	public CommandCommon(){
+		this.aliases = new ArrayList();
+		this.aliases.add("redox");
+		this.aliases.add("redoxiation");
+	}
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return "redoxiation";
+	}
+	@Override
+	public String getCommandUsage(ICommandSender p_71518_1_) {
+		// TODO Auto-generated method stub
+		return "redoxiation <text>";
+	}
+	@Override
+	public List getCommandAliases() {
+		// TODO Auto-generated method stub
+		return this.aliases;
+	}
+	@Override
+	public void processCommand(ICommandSender icommandsender, String[] argString) {
+		if(icommandsender instanceof EntityPlayer)
+		{
+			EntityPlayer player = (EntityPlayer) icommandsender;
+			if(argString.length == 0)
+			{
+				player.addChatMessage(new ChatComponentTranslation("[Redoxiation] Hello,  "+player.getGameProfile().getName()));
+			}
+		}
+		
+	}
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender p_71519_1_) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public List addTabCompletionOptions(ICommandSender p_71516_1_,
+			String[] p_71516_2_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isUsernameIndex(String[] p_82358_1_, int p_82358_2_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
