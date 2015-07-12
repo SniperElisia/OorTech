446cbe58127ca81c14efe920d7602d4081de0537
alvin137 <alvin137@naver.coom>
2015-07-12 09:08:15 +0900

    도전과제에

--- main/java/teamoort/redoxiation/achievement/AchievementEvents.java
+++ main/java/teamoort/redoxiation/achievement/AchievementEvents.java
@@ -1,22 +1,39 @@
 package teamoort.redoxiation.achievement;
 
+import teamoort.redoxiation.blocks.RedoxiationBlocks;
+import teamoort.redoxiation.blocks.RedoxiationOre;
 import teamoort.redoxiation.items.RedoxiationGenericItems;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.toposort.TopologicalSort;
 
 public class AchievementEvents {
 	@SubscribeEvent
 	public void entitySlain (LivingDeathEvent event)
 	{
-		if(event.source != null && event.source.getEntity() instanceof EntityPlayer)
+		/*if(event.source != null && event.source.getEntity() instanceof EntityPlayer)
 		{
 			EntityPlayer murderer = (EntityPlayer) event.source.getEntity();
 			
 			if(murderer.getHeldItem().getItem() != null)
 			{
-				RedoxiationAchievements.triggerAchievement(murderer, "START:DASH");
+				RedoxiationAchievements.triggerAchievement(murderer, "redoxiation.start");
+			}
+		}*/
+	}
+	
+	@SubscribeEvent
+	public void onBlockHarvest(HarvestDropsEvent event)
+	{
+		if(event.harvester instanceof EntityPlayer)
+		{
+			EntityPlayer miner = event.harvester;
+			
+			if(event.block == RedoxiationBlocks.CopperOre)
+			{
+				RedoxiationAchievements.triggerAchievement(miner, "redoxiation.start");
 			}
 		}
 	}
