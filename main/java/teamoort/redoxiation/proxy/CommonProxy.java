e3325e81f89d1be6c567aa5726a045f1294200bc
Malang Baram <malangbalam@darklambs.net>
2015-07-17 14:50:15 +0900

    가독성향상 프로젝트 -  프록시

--- main/java/teamoort/redoxiation/proxy/CommonProxy.java
+++ main/java/teamoort/redoxiation/proxy/CommonProxy.java
@@ -3,12 +3,16 @@ package teamoort.redoxiation.proxy;
 import teamoort.redoxiation.Redoxiation;
 
 public class CommonProxy {
-	public void registerTileEntitySpecialRenderer(){method();}
-	
-	private void method(){
-		if(Redoxiation.dummybool)
-			Redoxiation.logger.info("Tileentity proxy complete. Starting Recipe & Smelting ");
+	public void registerTileEntitySpecialRenderer() {
+		method();
+	}
+
+	private void method() {
+		if (Redoxiation.dummybool)
+			Redoxiation.logger
+					.info("Tileentity proxy complete. Starting Recipe & Smelting ");
 		else
-			Redoxiation.logger.info("Tileentity proxy complete. Starting World Generator");
+			Redoxiation.logger
+					.info("Tileentity proxy complete. Starting World Generator");
 	}
 }
