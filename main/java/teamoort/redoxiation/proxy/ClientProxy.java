b7d3fa5aced50aecde123895cf31fa9d787fbe37
Mixtilinear <teamoort@daum.net>
2015-07-23 21:10:55 +0900

    하나 수정

--- main/java/teamoort/redoxiation/proxy/ClientProxy.java
+++ main/java/teamoort/redoxiation/proxy/ClientProxy.java
@@ -1,14 +1,17 @@
 package teamoort.redoxiation.proxy;
 
 import teamoort.redoxiation.blocks.RedoxiationBlocks;
+import teamoort.redoxiation.blocks.tileentity.TileEntityExampleCog;
 import teamoort.redoxiation.blocks.tileentity.TileEntityIronCog;
 import teamoort.redoxiation.blocks.tileentity.TileEntityWoodenCog;
 import teamoort.redoxiation.blocks.tileentity.TileEntityStoneCog;
 import teamoort.redoxiation.render.ItemRenderIronCog;
 import teamoort.redoxiation.render.ItemRenderStoneCog;
+import teamoort.redoxiation.render.ItemRenderExampleCog;
 import teamoort.redoxiation.render.RenderIronCog;
 import teamoort.redoxiation.render.RenderWoodenCog;
 import teamoort.redoxiation.render.RenderStoneCog;
+import teamoort.redoxiation.render.RenderExampleCog;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.Item;
@@ -20,10 +23,13 @@ public class ClientProxy extends CommonProxy{
 		TileEntitySpecialRenderer renderWoodenCog = new RenderWoodenCog();
 		TileEntitySpecialRenderer renderStoneCog = new RenderStoneCog();
 		TileEntitySpecialRenderer renderIronCog = new RenderIronCog();
+        TileEntitySpecialRenderer renderExampleCog = new RenderExampleCog();
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityWoodenCog.class, renderWoodenCog);
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityStoneCog.class, renderStoneCog);
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityIronCog.class, renderIronCog);
+        ClientRegistry.bindTileEntitySpecialRenderer(TileEntityExampleCog.class, renderExampleCog);
 		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(RedoxiationBlocks.StoneCog), new ItemRenderStoneCog(renderStoneCog, new TileEntityStoneCog()));
 		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(RedoxiationBlocks.IronCog), new ItemRenderIronCog(renderIronCog, new TileEntityIronCog()));
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(RedoxiationBlocks.ExampleCog), new ItemRenderExampleCog(renderExampleCog, new TileEntityExampleCog()));
 	}
 }
